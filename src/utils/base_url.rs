a5c776dc1d609535e46eeea9415b50587c269e0b
Oforkaja Joseph <oforkajajoseph@gmail.com>
2026-01-12 16:52:47 +0100

    tried to fix the 404 error for other routes

--- src/utils/base_url.rs
+++ src/utils/base_url.rs
@@ -3,8 +3,8 @@
 
 pub struct BaseUrl;
 impl BaseUrl{
-    const BASE_URL: &str = "https://close-siusan-jaesea-21c201ce.koyeb.app";
-    // const BASE_URL: &str = "http://localhost:8000";
+    // const BASE_URL: &str = "https://close-siusan-jaesea-21c201ce.koyeb.app";
+    const BASE_URL: &str = "http://localhost:8000";
     pub fn get_base_url() -> String {
         option_env!("BASE_URL").map(|res| res.to_string()).unwrap_or_else(|| Self::BASE_URL.to_string())
     }

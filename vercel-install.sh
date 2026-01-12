1fe37c4984f74ae53bc58344413808069ffbaa7e
Oforkaja Joseph <oforkajajoseph@gmail.com>
2026-01-12 03:06:17 +0100

    still..

--- vercel-install.sh
+++ vercel-install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# Install Rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source . $HOME/.cargo/env
+set -e 
 
-# Add WASM target
+# 1. Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# 2. Update PATH for the rest of THIS script
+. "$HOME/.cargo/env"
+
+# 3. Add WASM target
 rustup target add wasm32-unknown-unknown
 
-# Download Trunk binary (faster than cargo install)
-wget -qO- https://github.com/trunk-rs/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
-mv trunk ./trunk_bin
+# 4. Download Trunk binary (using curl instead of wget)
+curl -L https://github.com/trunk-rs/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xz
 
-# Install Node dependencies (Tailwind, etc.)
+# 5. Move trunk and ensure it is executable
+mv trunk ./trunk_bin
+chmod +x ./trunk_bin
+
+# 6. Install Node dependencies
 npm install

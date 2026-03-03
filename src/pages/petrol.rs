50d0acebafe014d8f9ca61803d99a3d3b73753ec
Joseph <jaesea@Josephs-MacBook-Air.local>
2026-03-03 13:29:24 +0100

    added error message for location access denied

--- src/pages/petrol.rs
+++ src/pages/petrol.rs
@@ -68,14 +68,14 @@ pub fn Home_Petrol() -> impl IntoView {
                         }
                     },
                     Some(Err(e)) => {
-                        if e.contains("Geolocation failed") {
+                        if e.contains("GPS took too long or permission was denied") {
                             view! {
                                 <p class="error-msg">
-                                    "Location access was denied. "
-                                    "Please enable it in your browser settings and reload."
+                                    "Something went wrong! It could be that location access was denied."<br/>
+                                    "Please make sure location access is **enabled** in your browser settings and retry."
                                 </p>
                             }.into_any()
-                        }else if e.contains("outside the Abuja service area") {
+                        }else if e.contains("Seems you are outside the Abuja service area") {
                              view! { <p class="error-msg">{format!("Oops! {e}")}</p> }.into_any()
                         } 
                         else {

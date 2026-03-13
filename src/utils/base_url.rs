4f541522de5b83e612986c99c1ea9a67e0184bc1
Joseph <jaesea@Josephs-MacBook-Air.local>
2026-03-13 18:57:56 +0100

    styled landing and worked on cors

--- src/utils/base_url.rs
+++ src/utils/base_url.rs
@@ -1,11 +1,14 @@
-
-// pub const BASE_URL: &str = "https://close-siusan-jaesea-21c201ce.koyeb.app";
-
 pub struct BaseUrl;
 impl BaseUrl{
-    // const BASE_URL: &str = "https://close-siusan-jaesea-21c201ce.koyeb.app";
-    const BASE_URL: &str = "http://localhost:8080";
     pub fn get_base_url() -> String {
-        option_env!("BASE_URL").map(|res| res.to_string()).unwrap_or_else(|| Self::BASE_URL.to_string())
+        option_env!("BASE_URL")
+            .map(|res| res.to_string())
+            .unwrap_or_else(|| {
+                if cfg!(debug_assertions) {
+                    "http://localhost:8080".to_string()
+                } else {
+                    "https://close-siusan-jaesea-21c201ce.koyeb.app".to_string()
+                }
+            })
     }
 }
